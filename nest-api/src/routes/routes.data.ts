e2fa3a38dd96b0d238453ad3294d33cd6c2f4bb7
Luiz Almeida <luizorst@hotmail.com>
2021-04-07 17:48:01 -0300

    working backend

---
+++ nest-api/src/routes/routes.data.ts
@@ -0,0 +1,27 @@
+const routeOne = {
+  title: 'Rota 1',
+  startPosition: { lat: -15.82594, lng: -47.92923 },
+  endPosition: { lat: -15.82942, lng: -47.92765 },
+};
+const routeTwo = {
+  title: 'Rota 2',
+  startPosition: { lat: -15.82449, lng: -47.92756 },
+  endPosition: { lat: -15.8276, lng: -47.92621 },
+};
+const routeThree = {
+  title: 'Rota 3',
+  startPosition: { lat: -25.82331, lng: -47.92588 },
+  endPosition: { lat: -55.82458, lng: -47.92532 },
+};
+const routeFour = {
+  title: 'Rota 4',
+  startPosition: { lat: 58.88731, lng: -47.92588 },
+  endPosition: { lat: -15.82998, lng: -47.92532 },
+};
+const routeFive = {
+  title: 'Rota 5',
+  startPosition: { lat: 54.82331, lng: -47.92588 },
+  endPosition: { lat: 23.82758, lng: -47.92532 },
+};
+
+export = { routeOne, routeTwo, routeThree, routeFour, routeFive };
